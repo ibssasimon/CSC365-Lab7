17b6c13ad545b10dafb3123aee9b4facfc3b8d6c
Simon Ibssa <ibssasimon@gmail.com>
2020-06-07 13:37:47 -0700

    establishing connections

--- FRequirements.java
+++ FRequirements.java
@@ -1,21 +1,64 @@
+import java.sql.*;
+
 public class FRequirements {
+    private Connection establishConnection() throws SQLException {
+        // Create user name, password, url
+        String url = System.getenv("HP_JDBC_URL");
+        String user = System.getenv("HP_JDBC_USER");
+        String pass = System.getenv("HP_JDBC_PW");
+        // establish to DB
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+            return conn;
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public void FR5() {
-    System.out.println("TODO");
+        try {
+            Connection conn = establishConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TODO");
     }
 
     public void FR4() {
-    System.out.println("TODO");
+        try {
+            Connection conn = establishConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TODO");
+
     }
 
     public void FR3() {
-    System.out.println("TODO");
+        try {
+            Connection conn = establishConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TODO");
     }
 
     public void FR2() {
-    System.out.println("TODO");
+        try {
+            Connection conn = establishConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TODO");
     }
 
     public void FR1() {
+        try {
+            Connection conn = establishConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("TODO");
     }
 }
